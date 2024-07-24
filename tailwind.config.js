a643dd25fc8e1a14ecfd20e1f8d0e09a02927471
Silas Owu <owusilasyp00@gmail.com>
2024-07-24 05:05:56 -0700

    setup: add devdependencies

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontFamily: {
+      'poppins': ["Poppins", "sans-serif"],
+      'crimsonpro':["Crimson Pro", "serif"],
+      'lora':["Lora", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
